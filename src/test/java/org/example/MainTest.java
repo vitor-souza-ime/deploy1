d23dfbd905c6aa59d795c3ed22c68bae22b42e52
vitor-souza-ime <141266090+vitor-souza-ime@users.noreply.github.com>
2025-11-07 16:01:37 -0300

    teste

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+    @Test
+    void testeSimples() {
+        assertTrue(true, "Teste básico executado com sucesso.");
+    }
+}
